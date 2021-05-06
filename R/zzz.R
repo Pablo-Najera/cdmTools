7599bea09f1009bf1e7fbe6ef86538063493a717
Pablo Nájera <p.najeraalvarez@gmail.com>
2021-05-06 12:43:51 +0200

    Fix zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,11 @@
 }
 
 StartWelcomeMessage <- function(){
-  paste0("==========================================================\n",
-         "cdmTools Package",
-         " [Version ", utils::packageDescription("cdmTools")$Version,
-         "; ",utils::packageDescription("cdmTools")$Date, "]\n",
-         "More information: https://github.com/Pablo-Najera/cdmTools\n",
-         ("==========================================================\n"))
+  paste(c("==========================================================\n",
+          "cdmTools Package",
+          " [Version ", utils::packageDescription("cdmTools")$Version,
+          "; ",utils::packageDescription("cdmTools")$Date, "]\n",
+          "More information: https://github.com/Pablo-Najera/cdmTools\n",
+          "==========================================================\n"),
+        sep="")
 }
